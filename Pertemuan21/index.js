ba49c66ba0d10910324353da1e1b6550e7638da3
FitriantiKanal <s21810534@student.unklab.ac.id>
2020-11-27 14:00:35 -0800

    seventeen commit

---
+++ Pertemuan21/index.js
@@ -0,0 +1,50 @@
+ 
+ //Variabel
+
+ //let & const
+
+//  let umur = 25;
+//  _umur = 26;
+//  let nilai = 95.5;
+//  let sudahMenikah = true;
+//  let nama = "John";
+// console.log(umur);
+// console.log($nama);
+
+// let alamat
+
+//  const tahunLahir = 2000;
+//  console.log(tahunLahir);
+
+ //operator
+
+ //Arithmetic Operator (+,-,*,/)
+
+//  let angka1 = 10;
+//  let angka2 = 20;
+//  console.log (angka1 + angka2);
+
+//  let namaDepan = "John";
+//  let namaBelakang = "Doe";
+//  console.log ("Nama Saya" + namaDepan + " "+ namaBelakang + "Umur saya adalah"+ umur + "tahun");
+
+//operator perbandingan
+
+// let angka1 = 10;
+// let angka2 = 9;
+
+// console.log (angka1 <= angka2);
+
+//ternary operator
+// let hasil = 5;
+// let hasil = angka < 0 ? "Negatif" : "positif";
+// console.log(hasil);
+
+// //operator logika
+// let kondisi1 = false;
+// let kondisi2 = true;
+
+// console.log(kondisi1 || kondisi2);
+
+// console.log(!kondisi1);
+
